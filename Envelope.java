5bc0058ed2f5f8473ed63f968890bc7b99137688
alexlalama <alexlalamaemail@gmail.com>
2024-11-27 23:55:35 -0500

    Java program that emulates/prints  an envelope

---
+++ Envelope.java
@@ -0,0 +1,23 @@
+/**
+* An envelope class to print out an envelope 
+* @author Alexandra Lalama
+* @version 20241122
+ */
+ public class Envelope 
+ {
+   public static void main(String[] args)
+   {     
+      
+ 
+      System.out.println("Alexandra N. Lalama");
+      System.out.println("27 California Drive");
+      System.out.println("CityIn Jersualem");
+      System.out.println();
+      System.out.println();
+      System.out.println();
+      System.out.println("                       Rochester Institute of Technology");
+      System.out.println("                       One Lomb Memorial Drive");
+      System.out.println("                       Rochester, NY 14623");
+      
+   }
+ }
