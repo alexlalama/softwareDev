6fcec4fd1e0e028ba8e66262139efb00a6bcbe09
alexlalama <alexlalamaemail@gmail.com>
2024-11-28 00:43:57 -0500

    java Mug program fill and drain functions

--- Mug.java
+++ Mug.java
@@ -10,10 +10,41 @@ public class Mug {
         this.color = color;
         this.phrase = phrase;
     }
+    /*
+     * fill method adds an amount of ounces to the fill level
+     * if filled over the size level returns the size
+     * returns the fill level
+     */
+    
+    public int fill(int ounces){
+        this.fillLevel += ounces;
+        if(ounces > this.size){
+            this.fillLevel = this.size;
+        }
+        return this.fillLevel;
+    }
+    /*
+     * drain method removes amount of ounces to the fill level
+     * if fill level goes below zero mug fill level is 0
+     * returns the fill level
+     */
+    public int drain(int ounces){
+        this.fillLevel -= ounces;
+        if(this.fillLevel < 0){
+            this.fillLevel = 0;
+        }
+        int drained = this.fillLevel -ounces;
+        return drained;
+    }
     public static void main(String[] args){
         Mug mug1 = new Mug(8, "red", "I miss JuiceWrld");
         System.out.println("Mug1 Attributes:"+ mug1.color+
         " "+ mug1.fillLevel+ " "+mug1.size+" "+ mug1.phrase);
+        mug1.fill(12);
+        System.out.println(mug1.fillLevel);
+        mug1.drain(3);
+        System.out.println(mug1.fillLevel);
+        
     }    
 }
 
