206eda466309b37c95b0cc20e011171fdf89eaec
alexlalama <alexlalamaemail@gmail.com>
2024-11-27 23:51:44 -0500

    using default constructors

--- mugs.py
+++ mugs.py
@@ -2,21 +2,22 @@ class Mug:
     '''Class to represent a Mug'''
     # attributes, each attribute ro variable is a field
     __slots__ = ['size', 'color', 'fill_level', 'phrase']
+    #creating a default constructor to 'guarantee that every object has a value for every attribute'
+
+    def __init__(self, size, color, phrase):
+        self.size = size
+        self.color = color
+        self.fill_level = 0
+        self.phrase = phrase
 
 
 def main():
-    mug1 = Mug()
-    mug1.color = "gray"
-    mug1.size = "12"
-    mug2 = Mug()
-    mug2.phrase = "I miss JuiceWrld"
-    mug2.color = "brown"
-    # attribute error because mug1 does not have an phrase attribute assigned to its instance of a class
-    # print(mug1.phrase)
-    # Mug1 Color: gray Mug1 Size: 12
-    print("Mug1 Color:",mug1.color,"Mug1 Size:", mug1.size)
-    #Mug1 Color: brown Mug1 Phrase: I miss JuiceWrld
-    print("Mug1 Color:",mug2.color,"Mug1 Phrase:",mug2.phrase)
+    mug1 = Mug(8, "red", "I miss JuiceWrld")
+    mug2 = Mug(12, "gray", "I miss school")
+    #Mug1: 8 red I miss JuiceWrld
+    print("Mug1: ",mug1.size,mug1.color, mug1.fill_level ,mug1.phrase)
+    #Mug2: 12 gray I miss school
+    print("Mug2",mug2.size,mug2.color, mug2.fill_level ,mug2.phrase)
        
 if __name__ == "__main__":
     main()
