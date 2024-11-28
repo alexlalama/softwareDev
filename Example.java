6e1bb43e896b7af620f331ad02af439b029b8730
alexlalama <alexlalamaemail@gmail.com>
2024-11-27 23:55:00 -0500

    Java program that presents the function system.out.print.ln

---
+++ Example.java
@@ -0,0 +1,20 @@
+/**
+ * Simplest program ever
+ * @author Alexandra Lalama
+ * @version 20241122
+ */
+ 
+ public class Example
+ {
+   public static void main (String[] args)
+   {
+      // Adds a return or new line with ln
+      System.out.println("A message.");
+      // Does not add an extra line
+      System.out.print("Alexandra ");
+      System.out.print("N. ");
+      System.out.print("Lalama");
+      System.out.println();
+
+   }
+ }
