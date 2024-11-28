9645cbcb511a5629aa02f40687c20b4ec95bc63b
alexlalama <alexlalamaemail@gmail.com>
2024-11-28 01:17:21 -0500

    encapulsation errors

--- Stahbucks.java
+++ Stahbucks.java
@@ -1,15 +1,19 @@
 public class Stahbucks{
     public static void main(String[] args){
-        Mug mug1 = new Mug(-10, "red", "I miss JuiceWrld");
-        System.out.println("Mug1 Attributes:"+ mug1.color+
-        " "+ mug1.fillLevel+ " "+mug1.size+" "+ mug1.phrase);
-        mug1.fill(12);
-        System.out.println(mug1.fillLevel);
-        mug1.drain(3);
-        System.out.println(mug1.fillLevel);
+        // made attributes private to showcase how encapulsation works...
+        // bugs, bugs and bugs!
         
+        /**
+         * Mug mug1 = new Mug(-10, "red", "I miss JuiceWrld");
+         *System.out.println("Mug1 Attributes:"+ mug1.color+
+            " "+ mug1.fillLevel+ " "+mug1.size+" "+ mug1.phrase);
+         * mug1.fill(12);
+         * System.out.println(mug1.fillLevel);
+         *   mug1.drain(3);
+         * System.out.println(mug1.fillLevel);
+        */
         // Practing encapulsation to see if attribute can be accessed from another class
-        System.out.println(mug1.getSize());
-        
+        // System.out.println(mug1.getSize());
+
     }   
 }
