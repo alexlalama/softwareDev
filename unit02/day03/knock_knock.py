2ef9d098f216d37c5b97ee7f6601da692b1be341
alexlalama <alexlalamaemail@gmail.com>
2024-12-19 18:39:37 -0500

    day03 repo

---
+++ unit02/day03/knock_knock.py
@@ -0,0 +1,11 @@
+def joke():
+    knock_knock = input("knock knock: ")
+    print(knock_knock)
+    little_old_lady = input("Little old lady: ")
+    print(little_old_lady)
+    print("I didn't know that you could yodel!")
+
+def main():
+    joke()
+
+joke()
