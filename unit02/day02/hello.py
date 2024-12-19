afb14f9290ad31b7df22de68c538c5e65d441fe7
alexlalama <alexlalamaemail@gmail.com>
2024-12-19 01:07:28 -0500

    adding comments (multi, single line and docstrings)

---
+++ unit02/day02/hello.py
@@ -0,0 +1,22 @@
+"""
+Hello World function and Customized Hello You function
+
+Alexandra N. Lalama
+"""
+
+def hello_world():
+    """
+    prints Hello, Again, World!!!
+    """
+    print("Hello, Again, World!!!") #using the print() function to return a literal string
+
+def hello_you():
+    """
+    Prompts the user for their name and prints 
+    a customized hello message with their name
+    """
+    name = input("Enter your name: ") #prompts the user for their name
+    print("Hello, ", name, "!", sep="") #prints Hello, name!
+
+hello_world() #call to a function
+hello_you()
