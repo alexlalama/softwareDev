494664d55b8c8b06d3f617bd52817853de6045ce
alexlalama <alexlalamaemail@gmail.com>
2024-12-19 01:21:25 -0500

    practiced arithmitic

--- unit02/day02/variables.py
+++ unit02/day02/variables.py
@@ -1,3 +1,7 @@
+"""
+types of variables
+alexandra n. lalama
+"""
 def variable_practice():
     age_in_months = 267
     number_of_days_in_a_year = 365
@@ -7,4 +11,21 @@ def variable_practice():
     print("Number of days in a year:", number_of_days_in_a_year)
     print("Name of first pet:", name_of_first_pet)
     print("First five digits of pi:",five_dig_of_pi)
+def expressions_practice():
+    """
+    practicing arithmitic 
+    """
+    literal = 'Literal String'
+    addition_eq = 4 +4
+    exponent_eq = 3*2
+    floor_division_eq = 12/4
+    mod_eq = 18/4 
+    pemdas = (4 + 2)*(3-2) # 6 
+    print(literal)
+    print(addition_eq)
+    print(exponent_eq)
+    print(floor_division_eq)
+    print(mod_eq)
+    print(pemdas)
 variable_practice()
+expressions_practice()
