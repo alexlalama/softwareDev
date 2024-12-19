0cae2b63ab459bbefda5eb16a09be97f5bac1130
alexlalama <alexlalamaemail@gmail.com>
2024-12-19 01:01:40 -0500

    rewriting hello world & hello you as functions

---
+++ unit02/day02/helllo.py
@@ -0,0 +1,9 @@
+def hello_world():
+    print("Hello, Again, World!!!")
+
+def hell_you():
+    name = input("Enter your name: ")
+    print("Hello, ", name, "!", sep="")
+
+hello_world()
+hell_you()
