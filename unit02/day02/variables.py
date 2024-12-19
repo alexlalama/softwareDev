39191a786bc4919e7c81b26f0ead87db55b037b9
alexlalama <alexlalamaemail@gmail.com>
2024-12-19 01:14:12 -0500

    learning variables

---
+++ unit02/day02/variables.py
@@ -0,0 +1,10 @@
+def variable_practice():
+    age_in_months = 267
+    number_of_days_in_a_year = 365
+    name_of_first_pet = "Munchie"
+    five_dig_of_pi = 3.14159
+    print("Age in months:", age_in_months)
+    print("Number of days in a year:", number_of_days_in_a_year)
+    print("Name of first pet:", name_of_first_pet)
+    print("First five digits of pi:",five_dig_of_pi)
+variable_practice()
