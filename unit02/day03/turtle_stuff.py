ab34076b1ed59567f57caa967a4d357e81d120c8
alexlalama <alexlalamaemail@gmail.com>
2024-12-19 23:00:11 -0500

    created paramterized colors for squares

--- unit02/day03/turtle_stuff.py
+++ unit02/day03/turtle_stuff.py
@@ -16,14 +16,16 @@ def turtle_state():
     """
     prints current state of turtle
     """
-    if turtle.isdown():
-        return True
-    else:
-        return False, turtle.heading(), turtle.xcor(), turtle.ycor()
+    return turtle.isdown(), turtle.heading(), turtle.xcor(), turtle.ycor()
 
 
-def square(size, angle):
+def square(size, angle, pencolor, fillcolor):
+    turtle.pensize(5)
+    turtle.bgcolor("yellow")
+    turtle.color(pencolor)
+    turtle.fillcolor(fillcolor)
     turtle.left(angle)
+    turtle.begin_fill()
     turtle.pendown()
     turtle.forward(size)
     turtle.left(90)
@@ -33,20 +35,24 @@ def square(size, angle):
     turtle.left(90)
     turtle.forward(size)
     turtle.left(90)
-    turtle.right(45)
+    turtle.right(angle)
     turtle.penup()
+    turtle.end_fill()
 def main():
     # print(turtle_state())
     # test_drive()
     # print(turtle_state())
     print(turtle_state())
-    square(50,95)
+    square(120,95, "orange", "gray")
     print(turtle_state())
+    
+
     print(turtle_state())
-    square(100, 100)
+    square(90, 100, "green", "purple")
     print(turtle_state())
+
     print(turtle_state())
-    square(150, 105)
+    square(50, 105, "red", "pink")
     print(turtle_state())
    
     input("Press enter to continue... ")
