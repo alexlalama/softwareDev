71c881dde9ecc9c8da1413f4c1eb5fe9784b9486
alexlalama <alexlalamaemail@gmail.com>
2024-12-22 22:00:48 -0500

    draw eye function

--- unit03/day01/smiley.py
+++ unit03/day01/smiley.py
@@ -1,3 +1,8 @@
+"""
+Drawing a smiley face
+
+"""
+
 import turtle
 def draw_circle(x_cor, y_cor, radius,fillcolor):
     turtle.penup()
@@ -10,7 +15,7 @@ def draw_circle(x_cor, y_cor, radius,fillcolor):
     turtle.penup()
     turtle.end_fill()
 
-def draw_centered_circle(x_cor, y_cor, radius,fillcolor):
+def draw_centered_circle(radius,fillcolor):
     # use x, y
     # go to x + radius
     turtle.penup()
@@ -26,15 +31,26 @@ def draw_centered_circle(x_cor, y_cor, radius,fillcolor):
     turtle.left(90)
     turtle.forward(radius)
     turtle.left(180)
-    
+
 def draw_smiley():
-    draw_centered_circle(100, 100, 150, "gray")
-    draw_centered_circle(100, 100, 20, "pink")
+    draw_centered_circle(150, "gray")
+    draw_centered_circle( 20, "pink")
+
+def tweak(speed,tracer):
+    turtle.speed(speed)
+    turtle.tracer(tracer)
+
+def draw_eye(x, y, radius, iris_color):
+    turtle.hideturtle()
+    draw_circle(x, y, radius, "white")
+    draw_circle(x, y+radius/2, radius/2, iris_color)
+    draw_circle(x, y*2,radius/4, "black")
+
 def main():
-    # draw_circle(20, 30, 59, "red")
-    # draw_circle(200, 230, 10, "black")
-    # draw_circle(100, 100, 70, "gray")
-    draw_smiley(20, "pink")
+    # turtle.speed(10)
+    # draw_smiley()
+    draw_eye(60, 30, 40, "brown")
+    
     input("Press enter to continue")
 
 main()
