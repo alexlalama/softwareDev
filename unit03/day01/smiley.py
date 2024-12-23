5ca757393710b4d2a2e5df32b7aa2ae35c6ac4ea
alexlalama <alexlalamaemail@gmail.com>
2024-12-23 09:42:21 -0500

    improved mouth y axis proportions

--- unit03/day01/smiley.py
+++ unit03/day01/smiley.py
@@ -15,11 +15,11 @@ def draw_circle(x_cor, y_cor, radius,fillcolor):
     turtle.penup()
     turtle.end_fill()
 
-def draw_centered_circle(radius,fillcolor):
+def draw_centered_circle(x, y,radius,fillcolor):
     # use x, y
     # go to x + radius
     turtle.penup()
-    turtle.goto(0,0)
+    turtle.goto(x,y)
     turtle.forward(radius)
     turtle.left(90)
     turtle.begin_fill()
@@ -32,11 +32,12 @@ def draw_centered_circle(radius,fillcolor):
     turtle.forward(radius)
     turtle.left(180)
 
-def draw_smiley(x,y, radius, head_fill_color, nose_fill_color):
-    draw_centered_circle(radius, head_fill_color)
-    draw_centered_circle(radius*.25, nose_fill_color)
+def draw_smiley(x,y, radius, head_fill_color, nose_fill_color, mouth_fill_color):
+    draw_centered_circle(x,y,radius, head_fill_color)
+    draw_centered_circle(x,y,radius*.10, nose_fill_color)
     draw_eye(radius*.35, radius*.25,radius*.25,"brown")
     draw_eye(-1*(radius*.35), radius*.25,radius*.25,"brown")
+    draw_mouth(x, y, radius, mouth_fill_color)
 def tweak(speed,tracer):
     turtle.speed(speed)
     turtle.tracer(tracer)
@@ -47,7 +48,9 @@ def draw_eye(x, y, radius, iris_color):
     draw_circle(x, y*2,radius/4, "black")
 
 def draw_mouth(x, y, radius, fill_color):
-    turtle.goto(x, (.25*y-(radius*.6)))
+    radius = radius *.6
+    y = -1*(radius*.25)
+    turtle.goto(x, y)
     turtle.forward(radius)
     turtle.left(90)
     turtle.begin_fill()
@@ -59,10 +62,9 @@ def draw_mouth(x, y, radius, fill_color):
     turtle.penup()
     turtle.end_fill()
 def main():
-    turtle.tracer(False)
-    draw_smiley(0,0, 150, "yellow", "pink")
-    turtle.speed(3)
-    draw_mouth(0, 0, 80, "black")
+    # turtle.tracer(False)
+    tweak(10,False)
+    draw_smiley(0,0, 100, "yellow", "pink","black")
     input("Press enter to continue")
 
 main()
