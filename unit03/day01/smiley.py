dbd92bbc22d314e4563f1a96dceab6d1df93ed88
alexlalama <alexlalamaemail@gmail.com>
2024-12-22 22:30:56 -0500

    added a mouth to smiley

--- unit03/day01/smiley.py
+++ unit03/day01/smiley.py
@@ -34,23 +34,34 @@ def draw_centered_circle(radius,fillcolor):
 
 def draw_smiley():
     draw_centered_circle(150, "yellow")
-    draw_centered_circle( 20, "pink")
+    draw_centered_circle(20, "pink")
 
 def tweak(speed,tracer):
     turtle.speed(speed)
     turtle.tracer(tracer)
 
 def draw_eye(x, y, radius, iris_color):
-    turtle.hideturtle()
     draw_circle(x, y, radius, "white")
     draw_circle(x, y+radius/2, radius/2, iris_color)
     draw_circle(x, y*2,radius/4, "black")
 
+def draw_mouth(x, y, radius, fill_color):
+    turtle.goto(x,y)
+    turtle.left(90)
+    turtle.begin_fill()
+    turtle.pendown()
+    turtle.fillcolor(fill_color)
+    turtle.circle(radius, -180)
+    turtle.left(90)
+    turtle.forward(radius)
+    turtle.penup()
+    turtle.end_fill()
 def main():
     turtle.speed(10)
     draw_smiley()
     draw_eye(60, 30, 40, "brown")
     draw_eye(-60, 30, 40, "brown")
+    draw_mouth(40,-60, 40, "black")
     input("Press enter to continue")
 
 main()
