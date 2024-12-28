2b6773ed72fe3d2807475e4028d811efa8631c84
alexlalama <alexlalamaemail@gmail.com>
2024-12-28 14:34:07 -0500

    learning assert function

---
+++ unit03/day03/testing.py
@@ -0,0 +1,17 @@
+"""
+Asserting 12/28/2024
+Alex Lalama
+"""
+
+def test_1():
+    # assertion error
+    assert False
+def test_2():
+    # runs
+    assert True
+
+def main():
+    # test_1()
+    test_2()
+
+main()
