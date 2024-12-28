777ef838e63ef13111fdec9bdaecc6af280873d5
alexlalama <alexlalamaemail@gmail.com>
2024-12-28 16:22:40 -0500

    added square area test 2

--- unit03/day03/activities_test.py
+++ unit03/day03/activities_test.py
@@ -2,7 +2,13 @@ def square_area(length):
     '''
     
     '''
-    return 0
+    value = length*length
+    return value
+
+def test_square_area_6():
+    value = square_area(6)
+    assert value == 36
+
 def test_square_area_8():
     value = square_area(8)
     assert value == 64
