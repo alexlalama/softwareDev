a80f7a5ed48358556345e9db3d6ba34bb9f9db4b
alexlalama <alexlalamaemail@gmail.com>
2024-12-28 17:27:39 -0500

    test a negative number

--- unit03/day03/activities_test.py
+++ unit03/day03/activities_test.py
@@ -2,6 +2,8 @@ def square_area(length):
     '''
     
     '''
+    # if length < 0:
+    #     return None
     value = length*length
     return value
 
@@ -12,3 +14,8 @@ def test_square_area_6():
 def test_square_area_8():
     value = square_area(8)
     assert value == 64
+
+def test_length_less_than_zero():
+
+    value = square_area(-2) 
+    assert None == value
