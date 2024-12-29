a3421e4cc6d2afe7a838bd4a90e1385f2236f747
alexlalama <alexlalamaemail@gmail.com>
2024-12-28 20:30:27 -0500

    guess too high test

--- unit03/day03/guessing_test.py
+++ unit03/day03/guessing_test.py
@@ -7,7 +7,7 @@ def check_guess(answer, guess):
     if guess < answer:
         return "Too low!"
     if guess > answer:
-        return 0
+        return "Too high!"
 def test_check_guess_range_low():
     guess = check_guess(2, -1)
     assert guess == "Guess out of range"
