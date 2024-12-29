f3a886df9877979cf581a970df1cab2640d9aa69
alexlalama <alexlalamaemail@gmail.com>
2024-12-28 19:47:25 -0500

    test for guess range low

--- unit03/day03/guessing_test.py
+++ unit03/day03/guessing_test.py
@@ -3,8 +3,8 @@ def check_guess(answer, guess):
     Checks player's guess 
     '''
     
-    return -1
+    return "Guess out of range"
 
 def test_check_guess_range_low():
     guess = check_guess(2, -1)
-    assert guess == -1
+    assert guess == "Guess out of range"
