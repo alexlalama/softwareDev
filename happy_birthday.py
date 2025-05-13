35685a9dcd3babf8be1bcbd26ead83465fbe7ffb
alexlalama <alexlalamaemail@gmail.com>
2025-05-13 16:24:55 -0400

    prompt birthday

---
+++ happy_birthday.py
@@ -0,0 +1,7 @@
+def main():
+    name = input("Enter name: ")
+    birth_month =input("Enter birth month: ")
+    birth_day = input("Enter birth day: ")
+    birth_year= input("Enter birth year: ")
+    print(name, " your birthday is ",birth_month, " ",birth_day," ", birth_year)
+main()
