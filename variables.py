02999e802444c8b13dc7429301b28feabf23156e
alexlalama <alexlalamaemail@gmail.com>
2025-05-13 17:04:49 -0400

    variables practice

---
+++ variables.py
@@ -0,0 +1,12 @@
+def variable_practice():
+    age_in_months = 12 * 22
+    number_of_days = 365
+    first_pet = "Munchie"
+    pi = 3.14159
+    print(age_in_months)
+    print(number_of_days)
+    print(first_pet)
+    print(pi)
+def main():
+    variable_practice()
+main()
