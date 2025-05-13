a3454c7571b4e1abed95d3c26e35e0fbb1f27f6b
alexlalama <alexlalamaemail@gmail.com>
2025-05-13 15:10:37 -0400

    Outputs an envelope

---
+++ Envelope.java
@@ -0,0 +1,18 @@
+public class Envelope
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Alex Lalama");
+      System.out.println("14 Portsmouth Dr");
+      System.out.println("Old Bridge, N.J. 08857");
+      System.out.println(""); 
+      System.out.println("");
+      System.out.println("");
+      System.out.println("");
+      System.out.println("                      Middlesex County College");
+      System.out.println("                      Somewhere in Edison ");
+      System.out.println("                      Edison, N.J. 08873                     ");
+
+      
+   }
+}
