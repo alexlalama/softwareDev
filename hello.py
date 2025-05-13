a3cddc49a899422d1066292337fa11a6dae7fbe9
alexlalama <alexlalamaemail@gmail.com>
2025-05-13 16:59:57 -0400

    hello messages

--- hello.py
+++ hello.py
@@ -1,10 +1,21 @@
+"""
+    Prints hello messages
+    Alex Lalama
+"""
 def hello():
-    print("Hello, world!")
+    """
+    Prints Hello, world
+    """
+    print("Hello, world !") #prints message
 def hello_you():
-    name = input("Enter name: ")
-    print("Hello, ", name)
+    """
+    Prompts a user for their name and prints a hello message
+    """
+    name = input("Enter name: ") # prompts user for name
+    print("Hello, ", name) #prints 'Hello, ' with the name
 
 def main():
+    
     hello_you()
 
 main()
