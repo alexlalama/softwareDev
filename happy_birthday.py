31a7323501a42eaad40268e50b1285dfbd4233c8
alexlalama <alexlalamaemail@gmail.com>
2025-05-13 16:25:46 -0400

    birthday function

--- happy_birthday.py
+++ happy_birthday.py
@@ -1,7 +1,9 @@
-def main():
+def bday_message():
     name = input("Enter name: ")
     birth_month =input("Enter birth month: ")
     birth_day = input("Enter birth day: ")
     birth_year= input("Enter birth year: ")
-    print(name, " your birthday is ",birth_month, " ",birth_day," ", birth_year)
+    print(name, " your birthday is ",birth_month, birth_day,birth_year)
+def main():
+    bday_message()
 main()
