2ca35d29f959530d339d45636587ae2b1f2cdaa0
alexlalama <alexlalamaemail@gmail.com>
2025-05-15 18:56:11 -0400

    draw centered circle

--- python/smiley.py
+++ python/smiley.py
@@ -4,6 +4,9 @@ Alexandra Lalama
 """
 import turtle
 def draw_circle(x_cor, y_cor, radius, fill_color):
+    """
+    draws a circle
+    """
     turtle.penup()
     turtle.goto(x_cor,y_cor)
     turtle.begin_fill()
@@ -11,8 +14,19 @@ def draw_circle(x_cor, y_cor, radius, fill_color):
     turtle.circle(radius)
     turtle.end_fill()
 
+def draw_centered_circle():
+    """
+    draws a centered cirlce
+    """
+    # draw a circle
+    # forward radius
+    # turn forward radius
+    draw_circle(40, 50, 60, "purple")
+    
 def main():
-    draw_circle(30, 40, 70, "magenta")
+    #draw_circle(30, 40, 70, "magenta")
+    draw_centered_circle()
     input("Press enter to continue")
+
 main()
 
