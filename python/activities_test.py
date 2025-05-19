f62bdaab5692674df764218fe99a3c0b1663f95f
alexlalama <alexlalamaemail@gmail.com>
2025-05-19 16:15:53 -0400

    test_square_ area 6

--- python/activities_test.py
+++ python/activities_test.py
@@ -1,10 +1,18 @@
 def square_area(length):
     return 64
 
+
 def test_square_area_8():
     """
     find area of a square with side of eight
     """
     x = square_area(8)
     assert x == 64
+
+def test_square_area_6():
+    """
+    find area of a square with side of eight
+    """
+    x = square_area(6)
+    assert x == 36
 
