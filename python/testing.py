e0c6d8e3690b3d7e26111847b3e4619b76270157
alexlalama <alexlalamaemail@gmail.com>
2025-05-19 15:30:01 -0400

    test assert True and assert False

---
+++ python/testing.py
@@ -0,0 +1,14 @@
+def test_1():
+    
+    """
+    """
+    assert False #assertionError
+def test_2():
+    """
+    
+    """
+    assert True #no errors
+def main():
+    # test_1()
+    test_2()
+main()
