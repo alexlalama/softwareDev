fd8dbfaf25f3ca64bac6c331504fcdbf7c4119bc
alexlalama <alexlalamaemail@gmail.com>
2025-05-19 20:13:47 -0400

    negative test  pass

--- python/activities_test.py
+++ python/activities_test.py
@@ -1,5 +1,5 @@
 def square_area(length):
-    area = length * length
+    area = abs(length * length)
     return area 
 
 def test_square_area_8():
@@ -19,4 +19,4 @@ def test_square_area_6():
 def test_square_area_negative():
     # find area of negative square length
     x= square_area(-6)
-    assert None
+    assert x
