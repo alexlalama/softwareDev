5b2658ee88b23c2f987f6270fc7c85af3e53d17f
alexlalama <alexlalamaemail@gmail.com>
2025-05-22 14:04:23 -0400

    test check guess

---
+++ python/guessing.py
@@ -0,0 +1,22 @@
+import random
+def test_check_guess_range_low():
+    assert check_guess(2, 8) == 9
+    
+    
+def check_guess(guess, answer):
+    
+    '''
+    answer = random.randint(1, 10)
+    guess = int(input("Enter number 1 - 10"))
+    while(guess != answer):
+        if(guess >=10 ):
+            return "Out of range"
+        elif (guess != 10):
+            return 'Guess again'
+        else: 
+            return 0
+    ''' 
+
+    return ''
+    
+
