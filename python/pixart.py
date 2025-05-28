462e0dbd5f9033960a1ab1d2d932b997e57aff12
alexlalama <alexlalamaemail@gmail.com>
2025-05-28 16:08:53 -0400

    updated functions

---
+++ python/pixart.py
@@ -0,0 +1,26 @@
+PIXEL_SIZE = 30
+ROWS = 20
+COLUMNS = 20
+import turtle
+def pixart():
+    '''
+    '''
+    
+def speed():
+    return 0
+
+def xcor():
+    ''''''
+    return -300
+def ycor():
+    ''''''
+    return 300
+def isdown():
+    return False
+
+def pencolor():
+    return 'black'
+
+def fillcolor():
+    return 'white'
+
