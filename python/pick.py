60720c683a0e7fc6c0e52c16f0d410688660e378
alexlalama <alexlalamaemail@gmail.com>
2025-05-27 21:36:55 -0400

    if __name__ == main()

--- python/pick.py
+++ python/pick.py
@@ -11,21 +11,9 @@ def check_guess(guess, answer):
         return False
     else:
         return 0
-
-def test_check_guess_correct():
-    #assert
-    answer = 0
-    guess = 0
-    #invoke
-    
-    #analyze
-    assert check_guess(guess, answer) == True 
-def test_check_guess_incorrect():
-    answer =2 
-    guess = 3
-    assert check_guess(guess, answer) == False
 def main():
-    '''answer = random.randint(1, 10)
+    answer = random.randint(1, 10)
     guess = int(input("Enter a random number 1 - 10"))
-    print(is_correct(guess, answer))'''
-main()
+    print(is_correct(guess, answer))
+if __name__ == "main()":
+    main()
