6caefe74a4245c45640099f1c24c72a780fe71df
alexlalama <alexlalamaemail@gmail.com>
2025-05-28 16:08:30 -0400

    tests

--- python/pixart_test.py
+++ python/pixart_test.py
@@ -4,18 +4,18 @@ def test_speed():
     assert pixart.speed() == 0
 
 def test_xcor():
-    assert pixart.xcor() == 0
+    assert pixart.xcor() == -300
 
 
 def test_ycor():
-    assert pixart.ycor() == 0
+    assert pixart.ycor() == 300
 
 
 def test_isdown():
-    assert pixart.isdown() == 0
+    assert pixart.isdown() == False
 
 def test_pencolor():
-    assert pixart.pencolor() == 0
+    assert pixart.pencolor() == 'black'
 
 def test_fillcolor():
-    assert pixart.fillcolor ()== 0
+    assert pixart.fillcolor ()== 'white'
