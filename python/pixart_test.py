f75b461966c3cd4010bd3b0913d1c0cbf9de9ab0
alexlalama <alexlalamaemail@gmail.com>
2025-05-27 22:30:48 -0400

    tests for functions

---
+++ python/pixart_test.py
@@ -0,0 +1,21 @@
+import turtle
+import pixart
+def test_speed():
+    assert pixart.speed() == 0
+
+def test_xcor():
+    assert pixart.xcor() == 0
+
+
+def test_ycor():
+    assert pixart.ycor() == 0
+
+
+def test_isdown():
+    assert pixart.isdown() == 0
+
+def test_pencolor():
+    assert pixart.pencolor() == 0
+
+def test_fillcolor():
+    assert pixart.fillcolor ()== 0
