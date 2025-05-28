5366aab47eb4323ea7508ac9919fca2cad14a775
alexlalama <alexlalamaemail@gmail.com>
2025-05-27 20:39:22 -0400

    check guess test

---
+++ python/pick.py
@@ -0,0 +1,23 @@
+import random
+
+def is_correct(guess, answer):
+    return guess == answer
+
+
+def check_guess(guess, answer):
+    return None
+
+def test_check_guess_correct():
+    #assert
+    answer = 0
+    guess = 0
+    #invoke
+    
+    #analyze
+    assert check_guess(guess, answer) == 0 
+
+def main():
+    '''answer = random.randint(1, 10)
+    guess = int(input("Enter a random number 1 - 10"))
+    print(is_correct(guess, answer))'''
+main()
